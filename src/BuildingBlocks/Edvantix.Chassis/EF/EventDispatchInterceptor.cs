0bbdb3344a61c20b31d7370f630217b51ee7be86
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2025-12-07 21:18:19 +0300

    Добавлена базовая работа с EF

---
+++ src/BuildingBlocks/Edvantix.Chassis/EF/EventDispatchInterceptor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+using Edvantix.SharedKernel;
+using Edvantix.SharedKernel.SeedWork;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Edvantix.Chassis.EF;
+
+public sealed class EventDispatchInterceptor(IDomainEventDispatcher dispatcher)
+    : SaveChangesInterceptor
+{
+    public override async ValueTask<int> SavedChangesAsync(
+        SaveChangesCompletedEventData eventData,
+        int result,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var ctx = eventData.Context;
+
+        if (ctx is null)
+        {
+            return await base.SavedChangesAsync(eventData, result, cancellationToken);
+        }
+
+        var entitiesWithEvents = ctx
+            .ChangeTracker.Entries<IHasDomainEvents>()
+            .Select(e => e.Entity)
+            .Where(x => x.DomainEvents.Count != 0)
+            .ToImmutableList();
+
+        await dispatcher.DispatchAndClearEvents(entitiesWithEvents);
+
+        return await base.SavedChangesAsync(eventData, result, cancellationToken);
+    }
+}

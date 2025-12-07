26bd6c59c928ff776b5ba6762fb53062a1adfa3d
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2025-12-07 23:02:47 +0300

    Добавлена сборка ServiceDefaults

---
+++ src/Aspire/Edvantix.ServiceDefaults/Kestrel/HttpClientExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace Edvantix.ServiceDefaults.Kestrel;
+
+public static class HttpClientExtensions
+{
+    public static IHttpClientBuilder AddAuthToken(this IHttpClientBuilder builder)
+    {
+        builder.Services.TryAddTransient<HttpClientAuthorizationDelegatingHandler>();
+
+        builder.AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+
+        return builder;
+    }
+
+    private sealed class HttpClientAuthorizationDelegatingHandler(
+        IHttpContextAccessor httpContextAccessor
+    ) : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken
+        )
+        {
+            if (httpContextAccessor.HttpContext is not { } context)
+            {
+                return await base.SendAsync(request, cancellationToken);
+            }
+
+            var accessToken = await context.GetTokenAsync("access_token");
+
+            if (accessToken is not null)
+            {
+                request.Headers.Authorization = new(
+                    JwtBearerDefaults.AuthenticationScheme,
+                    accessToken
+                );
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}

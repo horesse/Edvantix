7c7dfcab873802695e5e530cdd1bb5e38713d80a
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2025-12-07 21:25:42 +0300

    Добавлена работа с контекстами

---
+++ src/BuildingBlocks/Edvantix.Chassis/EF/Configurations/EntityTypeConfigurationExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Edvantix.SharedKernel.SeedWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Edvantix.Chassis.EF.Configurations;
+
+public static class EntityTypeConfigurationExtensions
+{
+    public static void Configure<TEntity, TIdentity>(
+        this EntityTypeBuilder<TEntity> builder,
+        string scheme,
+        string table
+    )
+        where TIdentity : struct
+        where TEntity : Entity<TIdentity>
+    {
+        builder.ToTable(table, scheme);
+
+        builder.HasKey(bd => bd.Id);
+
+        builder.Property(bd => bd.Id).HasComment("Идентификатор");
+    }
+
+    public static void ConfigureSoftDeletable<TEntity, TIdentity>(
+        this EntityTypeBuilder<TEntity> builder,
+        string scheme,
+        string table
+    )
+        where TIdentity : struct
+        where TEntity : Entity<TIdentity>, ISoftDelete
+    {
+        builder.ToTable(table, scheme);
+
+        builder.HasKey(bd => bd.Id);
+
+        builder.Property(bd => bd.Id).HasComment("Идентификатор");
+
+        builder.Property(bd => bd.IsDeleted).HasComment("Признак удаленной записи");
+    }
+}

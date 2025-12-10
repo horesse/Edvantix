fc0e6158e8157e503031276eb0543dce00ea6d26
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2025-12-10 22:48:22 +0300

    fix

--- src/Aspire/Edvantix.ServiceDefaults/ApiSpecification/DocumentOptions.cs
+++ src/Aspire/Edvantix.ServiceDefaults/ApiSpecification/DocumentOptions.cs
@@ -5,9 +5,9 @@ public sealed class DocumentOptions
     public const string ConfigurationSection = "Document";
     public string Title { get; init; } = string.Empty;
     public string Description { get; init; } = string.Empty;
-    public string AuthorName { get; init; } = "Nhan Nguyen";
-    public string AuthorEmail { get; init; } = "nguyenxuannhan407@gmail.com";
-    public Uri AuthorUrl { get; init; } = new("https://github.com/foxminchan");
+    public string AuthorName { get; init; } = "Edvantix";
+    public string AuthorEmail { get; init; } = "deepheath322@gmail.com";
+    public Uri AuthorUrl { get; init; } = new("https://github.com/edvantix");
     public string LicenseName { get; init; } = "MIT";
     public Uri LicenseUrl { get; init; } = new("https://opensource.org/licenses/MIT");
 }

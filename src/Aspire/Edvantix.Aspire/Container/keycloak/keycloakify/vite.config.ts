c85885d0fb09e90e789a6ffa1987aa4a631ee09e
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2025-12-21 19:46:03 +0300

    tailwind 4, shadcn ui

--- src/Aspire/Edvantix.Aspire/Container/keycloak/keycloakify/vite.config.ts
+++ src/Aspire/Edvantix.Aspire/Container/keycloak/keycloakify/vite.config.ts
@@ -1,18 +1,26 @@
 import react from "@vitejs/plugin-react";
-import { keycloakify } from "keycloakify/vite-plugin";
-import { defineConfig } from "vite";
+import {keycloakify} from "keycloakify/vite-plugin";
+import {defineConfig} from "vite";
+import path from "path"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    keycloakify({
-      themeName: "edvantix",
-      themeVersion: "1.0.0",
-      groupId: "com.edvantix.edvantix.keycloak",
-      artifactId: "keycloak-theme-edvantix",
-      accountThemeImplementation: "none",
-      keycloakifyBuildDirPath: "../themes",
-    }),
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+        keycloakify({
+            themeName: "edvantix",
+            themeVersion: "1.0.0",
+            groupId: "com.edvantix.edvantix.keycloak",
+            artifactId: "keycloak-theme-edvantix",
+            accountThemeImplementation: "none",
+            keycloakifyBuildDirPath: "../themes",
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });

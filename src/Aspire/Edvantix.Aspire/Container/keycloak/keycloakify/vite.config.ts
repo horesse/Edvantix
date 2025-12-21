c4b28ec8a57b171f406cef2a8437f8d674810f31
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2025-12-21 20:01:51 +0300

    форматирование + обновление темы

--- src/Aspire/Edvantix.Aspire/Container/keycloak/keycloakify/vite.config.ts
+++ src/Aspire/Edvantix.Aspire/Container/keycloak/keycloakify/vite.config.ts
@@ -1,26 +1,26 @@
 import react from "@vitejs/plugin-react";
-import {keycloakify} from "keycloakify/vite-plugin";
-import {defineConfig} from "vite";
-import path from "path"
-import tailwindcss from '@tailwindcss/vite'
+import { keycloakify } from "keycloakify/vite-plugin";
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss(),
-        keycloakify({
-            themeName: "edvantix",
-            themeVersion: "1.0.0",
-            groupId: "com.edvantix.edvantix.keycloak",
-            artifactId: "keycloak-theme-edvantix",
-            accountThemeImplementation: "none",
-            keycloakifyBuildDirPath: "../themes",
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [
+    react(),
+    tailwindcss(),
+    keycloakify({
+      themeName: "edvantix",
+      themeVersion: "1.0.0",
+      groupId: "com.edvantix.edvantix.keycloak",
+      artifactId: "keycloak-theme-edvantix",
+      accountThemeImplementation: "none",
+      keycloakifyBuildDirPath: "../themes",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
 });

da73fbd9a30776a4f0a258cf8efd6bab0862f24a
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2026-01-22 22:40:39 +0300

    получение текущего профиля

---
+++ src/Services/Profile/Edvantix.ProfileService/Domain/AggregatesModel/ProfileAggregate/Specifications/ProfileByAccountSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Edvantix.Chassis.Specification.Builders;
+using Edvantix.Chassis.Specification.Generic;
+
+namespace Edvantix.ProfileService.Domain.AggregatesModel.ProfileAggregate.Specifications;
+
+public sealed class ProfileByAccountSpecification : AttributeSpecification<Profile>
+{
+    public ProfileByAccountSpecification(Guid accountId)
+    {
+        Query.Where(x => x.AccountId == accountId);
+    }
+}

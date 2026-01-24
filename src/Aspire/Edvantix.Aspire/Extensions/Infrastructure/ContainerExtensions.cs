cfa2900f75467c1df0fa7f0b3a956ff5a0d450b7
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2026-01-24 21:01:48 +0300

    chore(infra): replace PipelineExtensions with ContainerExtensions and integrate container registry

---
+++ src/Aspire/Edvantix.Aspire/Extensions/Infrastructure/ContainerExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Edvantix.Constants.Aspire;
+
+namespace Edvantix.Aspire.Extensions.Infrastructure;
+
+public static class ContainerExtensions
+{
+    /// <summary>
+    ///     Adds a container registry resource to the distributed application builder.
+    /// </summary>
+    /// <param name="builder">The distributed application builder to configure.</param>
+    /// <returns>
+    ///     An <see cref="IResourceBuilder{ContainerRegistryResource}" /> instance representing the configured container
+    ///     registry resource.
+    /// </returns>
+    public static IResourceBuilder<ContainerRegistryResource> AddContainerRegistry(
+        this IDistributedApplicationBuilder builder
+    )
+    {
+        var endpoint = builder
+            .AddParameter("container-endpoint")
+            .WithCustomInput(_ =>
+                new()
+                {
+                    Name = "ContainerEndpointParameter",
+                    Label = "Container Endpoint",
+                    InputType = InputType.Text,
+                    Description = "Enter your container registry endpoint here",
+                }
+            );
+
+        var repository = builder
+            .AddParameter("container-repository")
+            .WithCustomInput(_ =>
+                new()
+                {
+                    Name = "ContainerRepositoryParameter",
+                    Label = "Container Repository",
+                    InputType = InputType.Text,
+                    Description = "Enter your container registry repository here",
+                }
+            );
+
+        var registry = builder.AddContainerRegistry(
+            Components.ContainerRegistry,
+            endpoint,
+            repository
+        );
+
+        return registry;
+    }
+}

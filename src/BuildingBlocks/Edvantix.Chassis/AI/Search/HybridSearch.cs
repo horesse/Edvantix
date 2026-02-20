eb0310c1cfc6d4f2ce43a2cd60ca26efbc411a06
Valiantsin Palkhouski <41633649+horesse@users.noreply.github.com>
2026-02-21 00:19:45 +0300

    chore: добавлена поддержка Qdrant, интерфейсы для поиска и источников данных

---
+++ src/BuildingBlocks/Edvantix.Chassis/AI/Search/HybridSearch.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.VectorData;
+
+namespace Edvantix.Chassis.AI.Search;
+
+public sealed class HybridSearch(
+    IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator,
+    VectorStoreCollection<Guid, TextSnippet> collection
+) : ISearch
+{
+    public async Task<IReadOnlyList<TextSnippet>> SearchAsync(
+        string text,
+        ICollection<string> keywords,
+        int maxResults = 20,
+        CancellationToken cancellationToken = default
+    )
+    {
+        await collection.EnsureCollectionExistsAsync(cancellationToken);
+
+        var vectorCollection = (IKeywordHybridSearchable<TextSnippet>)collection;
+
+        var vector = await embeddingGenerator.GenerateVectorAsync(
+            text,
+            cancellationToken: cancellationToken
+        );
+
+        var options = new HybridSearchOptions<TextSnippet>
+        {
+            VectorProperty = r => r.Vector,
+            AdditionalProperty = r => r.Content,
+        };
+
+        var nearest = vectorCollection.HybridSearchAsync(
+            vector,
+            keywords,
+            maxResults,
+            options,
+            cancellationToken
+        );
+
+        return await nearest.Select(result => result.Record).ToListAsync(cancellationToken);
+    }
+}

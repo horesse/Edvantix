96598902e204b76d266c6eadb2f1f1ea73be60cc
Valiantsin Palkhouski <41633649+horesse@users.noreply.github.com>
2026-02-21 00:19:45 +0300

    chore: добавлена поддержка Qdrant, интерфейсы для поиска и источников данных

---
+++ src/BuildingBlocks/Edvantix.Chassis/AI/Ingestion/IIngestionSource.cs
@@ -0,0 +1,7 @@
+﻿namespace Edvantix.Chassis.AI.Ingestion;
+
+public interface IIngestionSource<in T>
+    where T : class
+{
+    Task IngestDataAsync(T data, CancellationToken cancellationToken = default);
+}

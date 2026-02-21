113e2835132b851495db5b8b804251d9d66bc77a
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2026-02-21 12:06:19 +0300

    event bus

---
+++ src/BuildingBlocks/Edvantix.Chassis/EventBus/Dispatcher/EventDispatcher.cs
@@ -0,0 +1,22 @@
+﻿using Edvantix.SharedKernel.SeedWork;
+using MassTransit;
+
+namespace Edvantix.Chassis.EventBus.Dispatcher;
+
+public sealed class EventDispatcher(IBus bus, IEventMapper eventMapper) : IEventDispatcher
+{
+    public async Task DispatchAsync(
+        DomainEvent @event,
+        CancellationToken cancellationToken = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(@event);
+
+        var integrationEvent =
+            eventMapper.MapToIntegrationEvent(@event)
+            ?? throw new InvalidOperationException(
+                $"No integration event mapping found for '{@event.GetType().Name}'."
+            );
+        await bus.Publish(integrationEvent, cancellationToken);
+    }
+}
---
+++ src/BuildingBlocks/Edvantix.Chassis/EventBus/Dispatcher/IEventDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using Edvantix.SharedKernel.SeedWork;
+
+namespace Edvantix.Chassis.EventBus.Dispatcher;
+
+public interface IEventDispatcher
+{
+    Task DispatchAsync(DomainEvent @event, CancellationToken cancellationToken = default);
+}

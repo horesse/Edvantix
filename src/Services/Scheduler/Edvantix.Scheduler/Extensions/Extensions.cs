cc2312212b1d374aceb83f25cf29bebad7c515eb
Valiantsin Palkhouski <41633649+horesse@users.noreply.github.com>
2026-02-26 00:08:19 +0300

    feature: добавлен сервис расписания

---
+++ src/Services/Scheduler/Edvantix.Scheduler/Extensions/Extensions.cs
@@ -0,0 +1,58 @@
+﻿using OpenTelemetry.Trace;
+using Quartz.AspNetCore;
+
+namespace Edvantix.Scheduler.Extensions;
+
+internal static class Extensions
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        var services = builder.Services;
+
+        builder.AddEventBus(typeof(ISchedulerApiMarker));
+
+        services.AddQuartz(q =>
+        {
+            q.SchedulerName = nameof(Scheduler);
+            q.SchedulerId = $"{nameof(Edvantix)}-{nameof(Scheduler)}-{Environment.MachineName}";
+            q.UseDefaultThreadPool(tp => tp.MaxConcurrency = Environment.ProcessorCount);
+            q.AddJobAndTrigger<CleanUpSentEmailJob>(builder.Configuration);
+            q.AddJobAndTrigger<ResendErrorEmailJob>(builder.Configuration);
+        });
+
+        services.AddQuartzServer(q => q.WaitForJobsToComplete = true);
+
+        services.AddOpenTelemetry().WithTracing(t => t.AddQuartzInstrumentation());
+    }
+
+    private static void AddJobAndTrigger<TJob>(
+        this IServiceCollectionQuartzConfigurator quartz,
+        IConfiguration config,
+        Action<ITriggerConfigurator>? trigger = null
+    )
+        where TJob : IJob
+    {
+        var key = new JobKey(typeof(TJob).Name, nameof(Scheduler));
+
+        var configKey = $"{nameof(Quartz)}:{key.Name}";
+        var cronExpression = config[configKey];
+
+        if (string.IsNullOrWhiteSpace(cronExpression))
+        {
+            throw new ArgumentException(
+                $"Cron schedule not found in configuration for key: {configKey}"
+            );
+        }
+
+        quartz.AddJob<TJob>(opts => opts.WithIdentity(key));
+
+        quartz.AddTrigger(opts =>
+        {
+            opts.ForJob(key)
+                .WithIdentity(key.Name)
+                .WithCronSchedule(cronExpression, x => x.InTimeZone(TimeZoneInfo.Utc));
+
+            trigger?.Invoke(opts);
+        });
+    }
+}
---
+++ src/Services/Scheduler/Edvantix.Scheduler/Jobs/CleanUpSentEmailJob.cs
@@ -0,0 +1,11 @@
+﻿using Edvantix.Contracts;
+
+namespace Edvantix.Scheduler.Jobs;
+
+public sealed class CleanUpSentEmailJob(IBus bus) : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        await bus.Publish(new CleanUpSentEmailIntegrationEvent(), context.CancellationToken);
+    }
+}

a70ac5d89272a514ec03239c735c8baa2db413fd
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2026-03-09 00:02:12 +0300

    chore: обновлены зависимости и конфигурации для поддержки Tailwind, обновления Keycloak, улучшения alias и типов Node

--- src/Aspire/Edvantix.AppHost/Container/keycloak/keycloakify/vite.config.ts
+++ src/Aspire/Edvantix.AppHost/Container/keycloak/keycloakify/vite.config.ts
@@ -1,15 +1,23 @@
+import path from "path";
 import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     react(),
+      tailwindcss(),
     keycloakify({
       themeName: "edvantix",
       themeVersion: "1.0.0",
-      groupId: "com.edvantix.bookworm.keycloak",
+      groupId: "com.edvantix.edvantix.keycloak",
       artifactId: "keycloak-theme-edvantix",
       accountThemeImplementation: "none",
       keycloakifyBuildDirPath: "../themes",

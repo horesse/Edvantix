cc1c3ea04bacc3e410f88dd0aa5b9c64eae05e25
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2026-03-12 22:48:07 +0300

    миграция с MassTransit на Wolverine

--- src/Services/Persona/Edvantix.Persona/Infrastructure/Migrations/20260312194616_Migrate to Wolverine.Designer.cs
+++ src/Services/Persona/Edvantix.Persona/Infrastructure/Migrations/20260312194616_Migrate to Wolverine.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Edvantix.Persona.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Edvantix.Persona.Infrastructure.Migrations
 {
     [DbContext(typeof(PersonaDbContext))]
-    partial class PersonaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260312194616_Migrate to Wolverine")]
+    partial class MigratetoWolverine
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

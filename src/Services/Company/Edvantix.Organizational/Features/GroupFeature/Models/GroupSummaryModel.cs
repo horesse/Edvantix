3a53ba99221c5ebc23611ea8c35e10c039f8d7aa
Valiantsin Palkhouski <41633649+horesse@users.noreply.github.com>
2026-03-12 22:05:48 +0300

    feature: удалены сервисы организаций и подписок

--- src/Services/Company/Edvantix.Organizational/Features/GroupFeature/Models/GroupSummaryModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Edvantix.Organizational.Features.GroupFeature.Models;
-
-public sealed record GroupSummaryModel(
-    Guid Id,
-    Guid OrganizationId,
-    string Name,
-    string? Description,
-    string Role
-);
--- tests/Edvantix.ArchTests/Abstractions/BaseTest.cs
+++ tests/Edvantix.ArchTests/Abstractions/BaseTest.cs
@@ -4,11 +4,9 @@ using Edvantix.Catalog;
 using Edvantix.Chassis;
 using Edvantix.Constants;
 using Edvantix.Notification;
-using Edvantix.Organizational;
 using Edvantix.Persona;
 using Edvantix.Scheduler;
 using Edvantix.SharedKernel;
-using Edvantix.Subscriptions;
 
 namespace Edvantix.ArchTests.Abstractions;
 
@@ -16,14 +14,10 @@ public abstract class BaseTest
 {
     protected static readonly Assembly BlogAssembly = typeof(IBlogApiMarker).Assembly;
     protected static readonly Assembly CatalogAssembly = typeof(ICatalogApiMarker).Assembly;
-    protected static readonly Assembly OrganizationalAssembly =
-        typeof(IOrganizationalApiMarker).Assembly;
     protected static readonly Assembly PersonaAssembly = typeof(IPersonaApiMarker).Assembly;
     protected static readonly Assembly NotificationAssembly =
         typeof(INotificationApiMarker).Assembly;
     protected static readonly Assembly SchedulerAssembly = typeof(ISchedulerApiMarker).Assembly;
-    protected static readonly Assembly SubscriptionsAssembly =
-        typeof(ISubscriptionsApiMarker).Assembly;
     protected static readonly Assembly ChassisAssembly = typeof(IChassisMarker).Assembly;
     protected static readonly Assembly ConstantsAssembly = typeof(IConstantsMarker).Assembly;
     protected static readonly Assembly SharedKernelAssembly = typeof(ISharedKernelMarker).Assembly;

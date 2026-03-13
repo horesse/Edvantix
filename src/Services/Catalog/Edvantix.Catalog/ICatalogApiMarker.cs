a26b28766dd2a2fd08477802f042b26c571cfd92
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2026-03-13 22:37:56 +0300

    chore: удален каталог Edvantix.Catalog, включая миграции, настройки и gRPC протоколы

--- src/Services/Catalog/Edvantix.Catalog/ICatalogApiMarker.cs
+++
@@ -1,6 +0,0 @@
-namespace Edvantix.Catalog;
-
-/// <summary>
-/// Маркер сборки микросервиса Catalog для регистрации сервисов, валидаторов и репозиториев.
-/// </summary>
-public interface ICatalogApiMarker;
--- tests/Edvantix.ArchTests/Abstractions/BaseTest.cs
+++ tests/Edvantix.ArchTests/Abstractions/BaseTest.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using Edvantix.Blog;
-using Edvantix.Catalog;
 using Edvantix.Chassis;
 using Edvantix.Constants;
 using Edvantix.Notification;
@@ -13,7 +12,6 @@ namespace Edvantix.ArchTests.Abstractions;
 public abstract class BaseTest
 {
     protected static readonly Assembly BlogAssembly = typeof(IBlogApiMarker).Assembly;
-    protected static readonly Assembly CatalogAssembly = typeof(ICatalogApiMarker).Assembly;
     protected static readonly Assembly PersonaAssembly = typeof(IPersonaApiMarker).Assembly;
     protected static readonly Assembly NotificationAssembly =
         typeof(INotificationApiMarker).Assembly;

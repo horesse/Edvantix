74835d6b07b9368b529b543564340c7468ebe79d
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2026-04-12 21:35:47 +0300

    chore: упрощение `GlobalUsings` и рефакторинг `Extensions` в Scheduler

--- src/Services/Scheduler/Edvantix.Scheduler/GlobalUsings.cs
+++ src/Services/Scheduler/Edvantix.Scheduler/GlobalUsings.cs
@@ -1,5 +1,4 @@
 ﻿global using Edvantix.Chassis.EventBus;
-global using Edvantix.Scheduler.Jobs;
 global using Edvantix.Scheduler.Jobs.Listeners;
 global using Edvantix.ServiceDefaults;
 global using MassTransit;
